d1cadc4e0bda9b6526a02f2e1414e418792fd808
Steven Meyers <sjjmeyers@gmail.com>
2020-05-27 18:16:18 -0600

    Add login, dashboard, and session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+
+  get '/login', to: 'sessions#index'
+
+  get '/dashboard', to: 'dashboard#show'
+
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
 end
