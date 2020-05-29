7329b4cfa0a05f56a97bead4e3e0b096d9bf6a87
Steven Meyers <sjjmeyers@gmail.com>
2020-05-28 18:53:48 -0600

    Add current_user and require_user methods to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    render file: 'public/404', status: 404 unless current_user
+  end
+
 end
