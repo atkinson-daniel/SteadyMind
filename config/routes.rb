e8a9d2b9faaca204ae413759f28c491100e12b2c
Steven Meyers <sjjmeyers@gmail.com>
2020-05-28 19:02:42 -0600

    Add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/login', to: 'sessions#index'
+  get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#show'
 
