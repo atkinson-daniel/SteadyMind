0e2ba19c417e90542f2ccb5973e9f25840bfdb31
Javier Aguilar <jvr.agx@gmail.com>
2020-05-28 20:28:11 -0600

    Add mood to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :user do
     resources :dashboard, only: [:index]
+    resources :mood
   end
 end
