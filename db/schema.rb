08671fd133cb811cf3d36656b7d6b1c075f506fe
Javier Aguilar <jvr.agx@gmail.com>
2020-05-28 20:28:48 -0600

    Add mood and moodlog table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200529013235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "mood_logs", force: :cascade do |t|
+    t.bigint "mood_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mood_id"], name: "index_mood_logs_on_mood_id"
+  end
+
+  create_table "moods", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "mood_logs", "moods"
 end
