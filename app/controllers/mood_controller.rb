b33e1790a1213be4b84ce61357b55f1caafe01d7
Javier Aguilar <jvr.agx@gmail.com>
2020-05-30 10:02:51 -0600

    Add create and update method

---
+++ app/controllers/mood_controller.rb
@@ -0,0 +1,26 @@
+class MoodController < ApplicationController
+  def create
+    mood = UserMood.new(user: current_user, mood: Mood.find(mood_params[:id]))
+    if mood.save
+      flash[:success] = "Mood entry saved!"
+    else
+      flash[:error] = "There was an error!"
+    end
+    redirect_to dashboard_index_path
+  end
+
+  def update
+    if UserMood.update(params[:id], mood_id: mood_params[:id])
+      flash[:success] = "Mood entry updated!"
+    else
+      flash[:error] = "There was an error!"
+    end
+    redirect_to dashboard_index_path
+  end
+
+  private
+
+  def mood_params
+    params[:mood].permit(:id)
+  end
+end
