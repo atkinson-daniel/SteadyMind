475e174e0cbb2c9d84b6d555d6245fb720a83213
Javier Aguilar <jvr.agx@gmail.com>
2020-05-30 10:03:24 -0600

    Add test to for user edit mood

--- spec/features/user/user_can_log_mood_spec.rb
+++ spec/features/user/user_can_log_mood_spec.rb
@@ -1,17 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe 'As a user' do
+  before do
+    @mood1 = Mood.create(name: 'worried')
+    @mood2 = Mood.create(name: 'neutral_face')
+    @mood3 = Mood.create(name: 'smile')
+  end
   it 'once logged in, I\'m redirected to a page where I can log my mood.' do
-    visit user_dashboard_index_path
+    stub_omniauth
+
+    visit '/login'
+
+    click_on 'Sign In With Google'
+    user = User.last
+
+    expect(current_path).to eq('/dashboard')
 
     expect(page).to have_content 'Please share your mood today to see your profile. You can always edit it later.'
 
-    choose(option: 'happy')
+    choose(id: "mood_id_#{@mood3.id}")
     click_on 'Submit'
 
-    mood_entry = MoodLog.last
-    expect(mood_entry.user_id).to eq 1
-    expect(mood_entry.mood_id).to eq 3
-    expect(page).to have_content('Zen Quote')
+    mood_entry = UserMood.last
+    expect(mood_entry.user_id).to eq user.id
+    expect(mood_entry.mood_id).to eq @mood3.id
+  end
+  it 'if I already logged a mood, I can edit the mood from my dashboard.' do
+    stub_omniauth
+
+    visit '/login'
+
+    click_on 'Sign In With Google'
+    user = User.last
+
+    expect(current_path).to eq('/dashboard')
+
+    choose(id: "mood_id_#{@mood2.id}")
+    click_on 'Submit'
+
+    mood_entry = UserMood.last
+    expect(mood_entry.user_id).to eq user.id
+    expect(mood_entry.mood_id).to eq @mood2.id
+
+    expect(page). to have_content 'Edit Mood'
+    choose(id: "mood_id_#{@mood1.id}")
+    click_on 'Save'
+
+    mood_entry.reload
+    expect(mood_entry.user_id).to eq user.id
+    expect(mood_entry.mood_id).to eq @mood1.id
   end
 end
