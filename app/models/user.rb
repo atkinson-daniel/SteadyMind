e13b8a80d41631ad6f892c667acfe6830a6ab537
Steven Meyers <sjjmeyers@gmail.com>
2020-05-30 11:03:14 -0600

    Add uniqueness validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
-  validates :uid, presence: true
+  validates :email, uniqueness: true
+  validates :uid, uniqueness: true
 
   def self.from_omniauth(auth_info)
-    user = User.find_or_create_by(uid: auth_info[:uid])
+    user = User.find_or_create_by(uid: auth_info[:sub])
     user.email = auth_info[:email]
     user.name = auth_info[:given_name]
     user.uid = auth_info[:sub]
-    user.save!
+    user.save
     user
   end
 end
