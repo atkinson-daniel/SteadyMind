fcdb70a1e3d0d045e541eefd8061381817eeaf1c
Javier Aguilar <jvr.agx@gmail.com>
2020-05-30 13:44:20 -0600

    Display fetched quote on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   before_action :require_user
 
-  def index; end
+  def index
+    @quote = Quote.quote_of_the_day if current_user.logged_mood?
+  end
 
   def show; end
 end
