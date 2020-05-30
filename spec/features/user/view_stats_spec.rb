b5c8f0ee840f14e9b35183789c7a93cbd2adc5fe
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-30 17:13:52 -0500

    add test for stats

---
+++ spec/features/user/view_stats_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  it 'when I visit the stats page I see line graph' do
+    stub_omniauth
+
+    visit '/login'
+
+    click_on 'Sign In With Google'
+    user = User.last
+    mood1 = Mood.create(name: 'worried')
+    mood2 = Mood.create(name: 'neutral_face')
+    mood3 = Mood.create(name: 'smile')
+    mood4 = Mood.create(name: 'smile')
+    mood5 = Mood.create(name: 'smile')
+    mood_entry1 = UserMood.create(user_id: user.id, mood_id: mood1.id, created_at: 'Tue 26 May 2020 21:21:12 UTC +00:00', updated_at: 'Tue 26 May 2020 21:21:12 UTC +00:00')
+    mood_entry2 = UserMood.create(user_id: user.id, mood_id: mood2.id, created_at: 'Wed 27 May 2020 21:21:12 UTC +00:00', updated_at: 'Wed 27 May 2020 21:21:12 UTC +00:00')
+    mood_entry3 = UserMood.create(user_id: user.id, mood_id: mood3.id, created_at: 'Thu 28 May 2020 21:21:12 UTC +00:00', updated_at: 'Thu 28 May 2020 21:21:12 UTC +00:00')
+    mood_entry4 = UserMood.create(user_id: user.id, mood_id: mood4.id, created_at: 'Fri 29 May 2020 21:21:12 UTC +00:00', updated_at: 'Fri 29 May 2020 21:21:12 UTC +00:00')
+    mood_entry5 = UserMood.create(user_id: user.id, mood_id: mood5.id, created_at: 'Sat 30 May 2020 21:21:12 UTC +00:00', updated_at: 'Sat 30 May 2020 21:21:12 UTC +00:00')
+
+    expect(current_path).to eq('/dashboard')
+    find(:xpath, "//a[@href='/stats']").click
+    save_and_open_page
+  end
+end
