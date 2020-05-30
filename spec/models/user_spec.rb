f8b2b1e7a42cd33b9fe29df16fa5c1753b928520
Javier Aguilar <jvr.agx@gmail.com>
2020-05-30 11:47:48 -0600

    Add methods to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,24 @@ RSpec.describe User, type: :model do
         sub: '923987239724972'
       }
 
-      expect(User.from_omniauth(auth_info)).to eq(User.last) 
+      expect(User.from_omniauth(auth_info)).to eq(User.last)
+    end
+  end
+
+  describe 'instance methods' do
+    it '#logged_mood?' do
+      user = create(:user)
+      mood = create(:mood)
+      expect(user.logged_mood?).to eq false
+      UserMood.create(user: user, mood: mood)
+      expect(user.logged_mood?).to eq true
+    end
+    it '#mood_entry' do
+      user = create(:user)
+      mood = create(:mood)
+      UserMood.create(user: user, mood: mood)
+      expect(user.mood_entry.user_id).to eq user.id
+      expect(user.mood_entry.mood_id).to eq mood.id
     end
   end
 end
