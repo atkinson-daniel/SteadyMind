c1d9963870da5580396fd568e8a3087b4d369556
Javier Aguilar <jvr.agx@gmail.com>
2020-05-30 11:27:41 -0600

    Add spec test for application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
     when :notice then 'alert alert-info'
     when :success then 'alert alert-success'
     when :error then 'alert alert-error'
-    when :alert then 'alert alert-error'
     end
   end
 end
