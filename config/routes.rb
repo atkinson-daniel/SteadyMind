14fdc8f2fa604f59dc77366c21c0fa1ccc1657ef
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-30 16:17:18 -0600

    Add all resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
   root 'welcome#index'
 
   get '/login', to: 'sessions#index'
   get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#show'
+
+  get '/all_resources', to: 'all_resources#index'
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
 end
