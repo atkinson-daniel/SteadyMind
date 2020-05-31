3ad8a87d2ff837c1a118fbe1298fb4d3656cf3e8
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-31 17:28:42 -0600

    Removed namespacing for resources and now yoga, meditation, and motivational videos will be rendered from all_resources controller

--- app/controllers/all_resources_controller.rb
+++ app/controllers/all_resources_controller.rb
@@ -1,9 +1,9 @@
-class AllResources::YogaController < ApplicationController
+class AllResourcesController < ApplicationController
   before_action :require_user
 
   def index
     video_results = VideoResults.new
-    yoga_videos = video_results.create_videos('yoga')
+    yoga_videos = video_results.create_videos(params[:category])
 
     @playlist_videos = PlaylistFacade.new(yoga_videos, params[:video_id])
   end
