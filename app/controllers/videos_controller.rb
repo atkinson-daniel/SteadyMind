cffcba49319d93c977d123da9720526465064eb0
Javier Aguilar <jvr.agx@gmail.com>
2020-05-31 21:45:03 -0600

    Add Playlist facade

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,7 @@ class VideosController < ApplicationController
   before_action :require_user
 
   def index
-    @videos = current_user.suggested_videos
+    videos = current_user.suggested_videos
+    @playlist_videos = PlaylistFacade.new(videos, params[:video_id])
   end
 end
