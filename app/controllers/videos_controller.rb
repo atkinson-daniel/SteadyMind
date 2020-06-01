d1eb8260329b4d369a2aa2a51b3747b0e5847ae1
Javier Aguilar <jvr.agx@gmail.com>
2020-05-31 22:04:56 -0600

    Display video categories if param is provided

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,7 @@ class VideosController < ApplicationController
 
   def index
     videos = current_user.suggested_videos
+    videos = videos.select { |video| video.category == params[:category] } if params[:category]
     @playlist_videos = PlaylistFacade.new(videos, params[:video_id])
   end
 end
