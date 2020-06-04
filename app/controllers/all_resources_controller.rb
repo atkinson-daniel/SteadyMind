be6cc38969def2c55d9db271a747edc3230768f0
Steven Meyers <sjjmeyers@gmail.com>
2020-06-03 23:04:27 -0600

    Update index method in AllResourcesController

--- app/controllers/all_resources_controller.rb
+++ app/controllers/all_resources_controller.rb
@@ -2,7 +2,18 @@ class AllResourcesController < ApplicationController
   before_action :require_user
 
   def index
-    yoga_videos = VideoResults.new.create_videos(params[:category])
-    @playlist_videos = PlaylistFacade.new(yoga_videos, params[:video_id])
+    video_results = VideoResults.new.create_videos
+    params[:category] = 'yoga' if params[:category].nil?
+    videos = find_all_by_category(video_results, params[:category])
+    
+    @playlist_videos = PlaylistFacade.new(videos, params[:video_id])
+  end
+
+  private
+
+  def find_all_by_category(videos, category)
+    videos.find_all do |video| 
+      video.category == category
+    end
   end
 end
