6f5256ca12f8f6b578b4dd6c6c540e77734f6796
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-04 15:53:45 -0500

    add environment configs

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,17 @@
 require_relative 'application'
 
 # Initialize the Rails application.
+
+
+# Configuration for using SendGrid on Heroku
 Rails.application.initialize!
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app172527008@heroku.com",
+  :password => "uj2bw8fp9049",
+  :domain => "https://steadymind.herokuapp.com/",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
