6aa564139f35e0ed1d8229978b7396422e8b40a2
Steven Meyers <sjjmeyers@gmail.com>
2020-06-28 14:10:19 -0600

    Fix rubocop formatting offenses

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,9 +1,9 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'smtp.sendgrid.net'
+  default from: 'smtp.sendgrid.net'
 
   def send_reminder_email(user)
     @user = user
-    mail( :to => @user.email,
-    :subject => 'Have you logged your mood today?' )
+    mail(to: @user.email,
+         subject: 'Have you logged your mood today?')
   end
 end
